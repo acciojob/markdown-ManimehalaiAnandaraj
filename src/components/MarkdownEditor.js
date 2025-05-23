060a985ef36881130ef6f10485824c40f367412b
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-05-23 11:20:52 +0530

    Completed assignment

---
+++ src/components/MarkdownEditor.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect} from 'react'
+//import marked from 'marked';
+
+const MarkdownEditor = () => {
+    const [markdown,setMarkdown] = useState('#Hello World');
+    const [isLoading,setIsLoading]= useState(false);
+
+    useEffect(() => {
+
+    },[markdown]);
+
+    const handleMarkdownChange = (e) => {
+        setMarkdown(e.target.value);
+    }
+
+  return (
+    <div className='markdown-container'>
+        {isLoading && <div className='loading'>Loading...</div>}
+        <textarea
+        className='textarea'
+        value={markdown}
+        onChange={handleMarkdownChange}
+        placeholder='Enter markdown here...'
+    />
+    <div
+    className='preview'
+    dangerouslySetInnerHTML={{_html:marked(markdown)}}
+    />
+    </div>
+  )
+}
+
+export default MarkdownEditor
